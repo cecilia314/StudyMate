052250437b862b2d6e534ed2137a68551004785e
cecilia314 <lopezsisternacecilia@yahoo.com>
2025-04-03 08:32:46 -0700

    refactor: Fix Styles

--- frontend/src/apolloClient.ts
+++ frontend/src/apolloClient.ts
@@ -1,13 +1,6 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
-const SERVER_URI = 'http://10.0.0.237:4000/graphql'; // ADD YOUR LOCAL IP ADDRESS
-const str = /[X]/;
-
-if (SERVER_URI.search(str) !== -1) {
-  console.log(
-    "Please change the SERVER_URI for your IP address in 'apolloClient.ts'"
-  );
-}
+const SERVER_URI = 'http://localhost:4000/graphql'; // ADD YOUR LOCAL IP ADDRESS
 
 const client = new ApolloClient({
   link: new HttpLink({
